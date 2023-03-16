36cccedf68b39554eda7bd6000a9d08897cba224
amgad-rady <amgad.s.rady@gmail.com>
2023-03-16 11:37:21 -0400

    Restored VS clutter in .gitignore

--- premake5.lua
+++ premake5.lua
@@ -24,7 +24,6 @@ project "GLFW"
     }
 
     filter "system:windows"
-        buildoptions {"-std=c11","-lgdi32"}
         systemversion "latest"
         staticruntime "On"
 
@@ -48,5 +47,10 @@ project "GLFW"
             "_CRT_SECURE_NO_WARNINGS"
         }
 
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
